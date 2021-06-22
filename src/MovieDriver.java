df9ccab2a2f433d7a2b939264c431363c3953bdc
Benis Tambe <will.jenifer@yahoo.com>
2021-06-22 14:21:02 -0400

    added loop

--- src/MovieDriver.java
+++ src/MovieDriver.java
@@ -2,29 +2,45 @@ import java.util.Scanner;
 
 public class MovieDriver {
     public static void  main(String[] args){
+
+        String answer = "y";
         Scanner scanner = new Scanner(System.in);
 
         Movie movieObj = new Movie();
 
-        System.out.println("Enter the title of a movie");
+        do {
+            System.out.println("Enter the title of a movie");
 
-        String movieTitle = scanner.nextLine();
+            String movieTitle = scanner.nextLine();
 
-        movieObj.setTitle(movieTitle);
+            movieObj.setTitle(movieTitle);
 
+            System.out.println("Enter the movie's rating");
 
-        System.out.println("Enter the movie's rating");
+            String movieRating = scanner.nextLine();
 
-        String movieRating = scanner.nextLine();
+            movieObj.setRating(movieRating);
 
-        movieObj.setRating(movieRating);
+            System.out.println("How many tickets?");
 
-        System.out.println("How many tickets?");
+            int soldTickets = scanner.nextInt();
 
-        int soldTickets = scanner.nextInt();
+            movieObj.setSoldTickets(soldTickets);
 
-        movieObj.setSoldTickets(soldTickets);
+            System.out.println(movieObj.toString());
 
-        System.out.println(movieObj.toString());
+            System.out.println();
+            System.out.println();
+
+            System.out.println("Would you like to add another movie?");
+            System.out.println("type y for yes and n for no");
+            scanner.nextLine();
+            answer = scanner.nextLine();
+
+            if(answer.equals("n")){
+                System.out.println("Goodbye");
+            }
+
+        } while(answer.equals("y"));
     }
 }
